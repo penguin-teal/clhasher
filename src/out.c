e821cb4ab93db8a3fd0c11a7226101eadd7f05bf
penguin-teal <_penguin@tuta.io>
2023-11-02 20:59:32 -0500

    Use --annotate

--- src/out.c
+++ src/out.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include "appArgs.h"
 
 void printOut(uint8_t *buf, FILE *f, struct AppArgs *appArgs)
 {
     char fmt[6];
     fmt[0] = '%';
+    char annoPrefix[3];
 
     uint64_t lines = appArgs->bits / appArgs->splitBits;
 
@@ -14,12 +16,15 @@ void printOut(uint8_t *buf, FILE *f, struct AppArgs *appArgs)
     {
         case 8:
             fmtCh = 'o';
+            memcpy(annoPrefix, "0o", 3);
             break;
         case 10:
             fmtCh = 'u';
+            annoPrefix[0] = '\0';
             break;
         case 16:
             fmtCh = 'X';
+            memcpy(annoPrefix, "0x", 3);
             break;
         default:
             printf("Radix %u unsupported.\n", appArgs->radix);
@@ -34,6 +39,10 @@ void printOut(uint8_t *buf, FILE *f, struct AppArgs *appArgs)
             fmt[3] = '\0';
             for(uint64_t i = 0; i < lines; i++)
             {
+                if(appArgs->annotate)
+                {
+                    fprintf(f, "u32 [%lu]:\t", i * sizeof(uint32_t));
+                }
                 fprintf(f, fmt, ((uint32_t*)buf)[i]);
             }
             break;
@@ -44,6 +53,10 @@ void printOut(uint8_t *buf, FILE *f, struct AppArgs *appArgs)
             fmt[4] = '\0';
             for(uint64_t i = 0; i < lines; i++)
             {
+                if(appArgs->annotate)
+                {
+                    fprintf(f, "u64 [%lu]:\t", i * sizeof(uint64_t));
+                }
                 fprintf(f, fmt, ((uint64_t*)buf)[i]);
             }
             break;
